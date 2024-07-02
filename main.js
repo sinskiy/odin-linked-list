fddcfbed11179cc0753bbb0747d8584b1019949f
sinskiy <sinskiy@proton.me>
2024-07-02 16:07:01 +0300

    feat: add basic linked list manipulation functions

--- main.js
+++ main.js
@@ -1,11 +1,22 @@
 class LinkedList {
   constructor(headNode, tailValue) {
     this.head = headNode;
-    this.tail = new Node(tailValue);
+    this.tail = new ListNode(tailValue);
+  }
+  append(value) {
+    const newNode = new ListNode(value);
+    this.tail.nextNode = newNode;
+  }
+  prepend(value) {
+    const newNode = new ListNode(value, this.head);
+    this.head = newNode;
+  }
+  pop() {
+    this.head = this.head.nextNode;
   }
 }
 
-class Node {
+class ListNode {
   constructor(value, nextNode) {
     this.value = value ?? null;
     this.nextNode = nextNode ?? null;
